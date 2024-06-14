27f0a58fbc5c6eef268002fd5e66d3b5dc43a006
Yevhenii Babichenko <eugene.babichenko@protonmail.com>
2024-06-14 22:41:36 +0300

    fix(rules/cp_cwd): reacting on non-cp command

--- src/rules/cp_cwd.rs
+++ src/rules/cp_cwd.rs
@@ -1,9 +1,21 @@
 pub fn cp_cwd(cmd: &[String], _error: &str) -> Option<Vec<String>> {
-    if !cmd.contains(&"cd".to_string()) && !cmd.len() == 2 {
+    if !cmd.contains(&"cd".to_string()) || !cmd.len() == 2 {
         return None;
     }
 
     let mut cmd = cmd.to_vec();
     cmd.push(".".to_string());
     Some(cmd)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::cp_cwd;
+
+    #[test]
+    pub fn non_cp_cmd() {
+        let cmd = &["git".to_string(), "push".to_string()];
+        let error = "";
+        assert_eq!(None, cp_cwd(cmd, error));
+    }
 }
