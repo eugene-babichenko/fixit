c8432aab613e4f49a51ba86f90bc6cb730331c60
Yevhenii Babichenko <eugene.babichenko@protonmail.com>
2024-08-21 01:22:07 +0300

    fix(init): fix getting alias list for fish

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -67,11 +67,10 @@ function {name}() {{
                     "
 function {name} -d \"Fix your previous command\"
     set -l previous_cmd \"$history[1]\"
-    set -lx FIXIT_FNS \"(
+    set -lx FIXIT_FNS (
         functions | cut -d' ' -f1
         alias | cut -d' ' -f2
-    )\"
-    echo \"$FIXIT_FNS\"
+    )
     {executable} fix \"$previous_cmd\" | read -l fixed_cmd
     if [ \"$fixed_cmd\" != \"\" ]
         commandline \"$fixed_cmd\"
