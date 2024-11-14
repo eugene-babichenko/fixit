2c64daa340291295bdb1363e4cb5625101c117ac
Yevhenii Babichenko <eugene.babichenko@protonmail.com>
2024-11-14 07:51:33 +0200

    feat(get_text/iterm): use short form applescript

--- src/get_text/iterm.rs
+++ src/get_text/iterm.rs
@@ -3,13 +3,8 @@ use std::{env, process::Command};
 use super::find_command_output;
 
 /// This is an AppleScript that gets the contents of the current session
-const SCRIPT: &str = r#"
-tell application "iTerm2"
-    tell current session of current window
-        contents
-    end tell
-end tell
-"#;
+const SCRIPT: &str =
+    r#"tell application "iTerm2" to get contents of current session of current window"#;
 
 pub fn get_text(cmd: &str, depth: usize) -> Option<String> {
     env::var("ITERM_SESSION_ID").ok()?;
