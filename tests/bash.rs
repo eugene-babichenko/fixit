0e3be79b35fb589d5a1f9be04231917bbd7b2d71
Yevhenii Babichenko <eugene.babichenko@protonmail.com>
2024-11-15 11:10:55 +0200

    e2e bash test

---
+++ tests/bash.rs
@@ -0,0 +1,32 @@
+use std::time::Duration;
+use std::{env, process::Command};
+
+use expectrl::Session;
+use tempfile::NamedTempFile;
+
+#[test]
+fn bash() {
+    let histfile = NamedTempFile::new().unwrap();
+
+    let mut bash = Command::new("bash");
+    bash.args(["--norc", "-i", "-o", "history"])
+        .env(
+            "PATH",
+            &format!("./target/debug/:{}", env::var("PATH").unwrap()),
+        )
+        .env("FIXIT_QUICK_ENABLE", "false")
+        .env("HISTFILE", histfile.path())
+        .env("SHELL", "/bin/bash");
+
+    let mut p = Session::spawn(bash).expect("Failed to spawn bash");
+
+    p.set_expect_timeout(Some(Duration::from_secs(1000)));
+
+    p.send_line("eval \"$(fixit init bash)\"").unwrap();
+    p.send_line("eco 'Hello, world!'").unwrap();
+    p.expect("command not found").unwrap();
+    p.send_line("fix").unwrap();
+    p.send_line("").unwrap();
+    p.expect("Hello, world!").unwrap();
+    p.send_line("exit").unwrap();
+}
