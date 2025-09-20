c3673784db235e9343314a531b6c2260e883e0a4
Yevhenii Babichenko <eugene.babichenko@protonmail.com>
2025-09-20 03:35:57 +0300

    shellcheck run

--- templates/alias.sh
+++ templates/alias.sh
@@ -1,11 +1,14 @@
 function __name__() {
-    local previous_cmd="$(fc -ln -1)"
-    local FIXIT_FNS="$(
+    local previous_cmd
+    previous_cmd="$(fc -ln -1)"
+    local FIXIT_FNS
+    FIXIT_FNS="$(
         declare -F | cut -d' ' -f3
         alias | cut -d'=' -f1
     )"
     export FIXIT_FNS
-    local fixed_cmd="$(fixit fix "$previous_cmd")"
+    local fixed_cmd
+    fixed_cmd="$(fixit fix "$previous_cmd")"
     if [ "$fixed_cmd" != "" ]; then
         eval "$fixed_cmd"
 	history -s "$fixed_cmd"
--- templates/alias.zsh
+++ templates/alias.zsh
@@ -1,11 +1,14 @@
 function __name__() {
-    local previous_cmd="$(fc -ln -1)"
-    local FIXIT_FNS="$(
-        print -l ${(ok)functions}
+    local previous_cmd
+    previous_cmd="$(fc -ln -1)"
+    local FIXIT_FNS
+    FIXIT_FNS="$(
+        print -l "${(ok)functions}"
         alias | cut -d'=' -f1
     )"
     typeset -x FIXIT_FNS
-    local fixed_cmd="$(fixit fix "$previous_cmd")"
+    local fixed_cmd
+    fixed_cmd="$(fixit fix "$previous_cmd")"
     if [ "$fixed_cmd" != "" ]; then
         eval "$fixed_cmd"
 	print -s "$fixed_cmd"
